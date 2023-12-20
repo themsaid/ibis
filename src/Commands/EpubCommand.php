93ddcbff24da24822f8027b7390d4d8135c7a670
Roberto Butti <roberto.butti@gmail.com>
2023-12-20 23:36:46 +0100

    EPUB generation

--- src/Commands/EpubCommand.php
+++ src/Commands/EpubCommand.php
@@ -13,7 +13,6 @@ use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Symfony\Component\Console\Command\Command;
-
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
 use League\CommonMark\Extension\CommonMark\Node\Block\IndentedCode;
@@ -26,9 +25,10 @@ use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
 use League\CommonMark\Extension\Table\TableExtension;
 use Symfony\Component\Console\Output\OutputInterface;
 use League\CommonMark\MarkdownConverter;
+use PHPePub\Core\EPub;
 use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
 
-class BuildCommand extends Command
+class EpubCommand extends Command
 {
     /**
      * @var string|string[]|null
@@ -53,9 +53,9 @@ class BuildCommand extends Command
     protected function configure()
     {
         $this
-            ->setName('build')
+            ->setName('epub')
             ->addArgument('theme', InputArgument::OPTIONAL, 'The name of the theme', 'light')
-            ->setDescription('Generate the book.');
+            ->setDescription('Generate the book in Epub format.');
     }
 
     /**
@@ -69,6 +69,12 @@ class BuildCommand extends Command
      */
     public function execute(InputInterface $input, OutputInterface $output): int
     {
+        $this->output = $output;
+        $this->output->writeln('✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨');
+        $this->output->writeln('<info>✨ EPUB generation is a Working in Progress!! ✨</info>');
+        $this->output->writeln('<info>✨                Stay tuned!!                ✨</info>');
+        $this->output->writeln('✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨');
+        //return 0;
         $this->disk = new Filesystem();
         $this->output = $output;
         $this->themeName = $input->getArgument('theme');
@@ -87,7 +93,7 @@ class BuildCommand extends Command
 
         $theme = $this->getTheme($currentPath, $this->themeName);
 
-        $this->buildPdf(
+        $result = $this->buildEpub(
             $this->buildHtml($currentPath . '/content', $config),
             $config,
             $currentPath,
@@ -95,7 +101,13 @@ class BuildCommand extends Command
         );
 
         $this->output->writeln('');
-        $this->output->writeln('<info>Book Built Successfully!</info>');
+        if ($result) {
+
+            $this->output->writeln('<info>Book Built Successfully!</info>');
+        } else {
+            $this->output->writeln('<error>Book Built Failed!</error>');
+        }
+
 
         return 0;
     }
@@ -123,6 +135,7 @@ class BuildCommand extends Command
      */
     protected function buildHtml(string $path, array $config)
     {
+
         $this->output->writeln('<fg=yellow>==></> Parsing Markdown ...');
 
 
@@ -207,7 +220,7 @@ class BuildCommand extends Command
      * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      * @throws \Mpdf\MpdfException
      */
-    protected function buildPdf(Collection $chapters, array $config, string $currentPath, string $theme)
+    protected function buildEpub(Collection $chapters, array $config, string $currentPath, string $theme)
     {
         $defaultConfig = (new ConfigVariables())->getDefaults();
         $fontDirs = $defaultConfig['fontDir'];
@@ -215,31 +228,48 @@ class BuildCommand extends Command
         $defaultFontConfig = (new FontVariables())->getDefaults();
         $fontData = $defaultFontConfig['fontdata'];
 
-        $pdf = new Mpdf([
-            'mode' => 'utf-8',
-            'format' => $config['document']['format'] ?? [210, 297],
-            'margin_left' => $config['document']['margin_left'] ?? 27,
-            'margin_right' => $config['document']['margin_right'] ?? 27,
-            'margin_bottom' => $config['document']['margin_bottom'] ?? 14,
-            'margin_top' => $config['document']['margin_top'] ?? 14,
-            'fontDir' => array_merge($fontDirs, [getcwd() . '/assets/fonts']),
-            'fontdata' => $this->fonts($config, $fontData),
-        ]);
 
-        $pdf->SetTitle(Ibis::title());
-        $pdf->SetAuthor(Ibis::author());
-        $pdf->SetCreator(Ibis::author());
-
-        $pdf->setAutoTopMargin = 'pad';
-
-        $pdf->setAutoBottomMargin = 'pad';
-
-        $tocLevels = $config['toc_levels'];
-
-        $pdf->h2toc = $tocLevels;
-        $pdf->h2bookmarks = $tocLevels;
-
-        $pdf->SetMargins(400, 100, 12);
+        $content_start =
+            "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+            . "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\"\n"
+            . "    \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n"
+            . "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
+            . "<head>"
+            . "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n"
+            . "<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\" />\n"
+            . "<title>Test Book</title>\n"
+            . "</head>\n"
+            . "<body>\n";
+        $content_start =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+        . "<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:epub=\"http://www.idpf.org/2007/ops\">\n"
+        . "<head>"
+        . "<meta http-equiv=\"Default-Style\" content=\"text/html; charset=utf-8\" />\n"
+        . "<link rel=\"stylesheet\" type=\"text/css\" href=\"github.css\" />\n"
+        . "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />\n"
+        . "<title>" . Ibis::title() . "</title>\n"
+        . "</head>\n"
+        . "<body>\n";
+        $book = new EPub();
+        $book->setTitle(Ibis::title());
+        $book->setAuthor(Ibis::author(), Ibis::author());
+        $book->setIdentifier(Ibis::title() . "&amp;stamp=" . time(), EPub::IDENTIFIER_URI);
+        $book->setLanguage("en");
+        //$cssData = "body {\n  margin-left: .5em;\n  margin-right: .5em;\n  text-align: justify;\n}\n\np {\n  font-family: serif;\n  font-size: 10pt;\n  text-align: justify;\n  text-indent: 1em;\n  margin-top: 0px;\n  margin-bottom: 1ex;\n}\n\nh1, h2 {\n  font-family: sans-serif;\n  font-style: italic;\n  text-align: center;\n  background-color: #6b879c;\n  color: yellow;\n  width: 100%;\n}\n\nh1 {\n    margin-bottom: 2px;\n}\n\nh2 {\n    margin-top: -2px;\n    margin-bottom: 2px;\n}\n";
+        //$book->addCSSFile("styles.css", "css1", $cssData);
+        $cssData = file_get_contents("assets/style.css");
+        //dd($cssData);
+        $book->addCSSFile("style.css", "css1", $cssData);
+        $cssData = file_get_contents("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.css");
+        //dd($cssData);
+        $book->addCSSFile("github.css", "css2", $cssData);
+        //$book->addChapter("Table of Contents", "TOC.xhtml", null, false, EPub::EXTERNAL_REF_IGNORE);
+        $cover = $content_start . "<h1>" . Ibis::title() . "</h1>\n";
+        if (Ibis::author()) {
+            $cover .= "<h2>By: " . Ibis::author() . "e</h2>\n";
+        }
+        $content_end = "</body></html>";
+        $cover .= $content_end;
         $coverImage = "cover.jpg";
         if (key_exists("image", $config['cover'])) {
             $coverImage = $config['cover']['image'];
@@ -249,59 +279,34 @@ class BuildCommand extends Command
 
             $coverPosition = $config['cover']['position'] ?? 'position: absolute; left:0; right: 0; top: -.2; bottom: 0;';
             $coverDimensions = $config['cover']['dimensions'] ?? 'width: 210mm; height: 297mm; margin: 0;';
-
-            $pdf->WriteHTML(
-                <<<HTML
-<div style="{$coverPosition}">
-    <img src="assets/{$coverImage}" style="{$coverDimensions}"/>
-</div>
-HTML
-            );
-
-            $pdf->AddPage();
-        } elseif ($this->disk->isFile($currentPath . '/assets/cover.html')) {
-            $this->output->writeln('<fg=yellow>==></> Adding Book Cover ...');
-
-            $cover = $this->disk->get($currentPath . '/assets/cover.html');
-
-            $pdf->WriteHTML($cover);
-
-            $pdf->AddPage();
-        } else {
-            $this->output->writeln('<fg=red>==></> No assets/' . $coverImage . ' File Found. Skipping ...');
+            $book->setCoverImage("Cover.jpg", file_get_contents("assets/{$coverImage}"), "image/jpeg");
         }
 
-        $pdf->SetHTMLFooter('<div id="footer" style="text-align: center">{PAGENO}</div>');
-
-        $this->output->writeln('<fg=yellow>==></> Building PDF ...');
-
-        $pdf->WriteHTML(
-            $theme
-        );
-
+        $book->addChapter("Notices", "Cover.html", $cover);
+        $book->buildTOC();
         foreach ($chapters as $key => $chapter) {
             $this->output->writeln('<fg=yellow>==></> ❇️ ' . $chapter["mdfile"] . ' ...');
-            if (array_key_exists('header', $config)) {
-                $pdf->SetHTMLHeader(
-                    '
-                    <div style="' . $config['header'] . '">
-                        ' . Arr::get($chapter, "frontmatter.title", $config["title"]) . '
-                    </div>'
-                );
-            }
-            $pdf->WriteHTML(
-                $chapter["html"]
+            $book->addChapter(
+                Arr::get($chapter, "frontmatter.title", "Chapter " . $key),
+                "Chapter" . $key . " .html",
+                $content_start . $chapter["html"] . $content_end
             );
+            //file_put_contents('export/' . "Chapter" . $key . " .html", $content_start . $chapter["html"] . $content_end);
         }
+        //$book->buildTOC();
 
-        $this->output->writeln('<fg=yellow>==></> Writing PDF To Disk ...');
+        $book->finalize();
+
+        $epubFilename = 'export/' . Ibis::outputFileName() . '-' . $this->themeName . '.epub';
+        $zipData = $book->saveBook($epubFilename);
+
+
+        $this->output->writeln('<fg=green>==></> EPUB file ' . $epubFilename . ' created');
+        return true;
+
+        $this->output->writeln('<fg=yellow>==></> Writing EPUB To Disk ...');
 
         $this->output->writeln('');
-        $this->output->writeln('✨✨ ' . $pdf->page . ' PDF pages ✨✨');
-
-        $pdf->Output(
-            $currentPath . '/export/' . Ibis::outputFileName() . '-' . $this->themeName . '.pdf'
-        );
     }
 
     /**
